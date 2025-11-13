9bcb24f8d1e3fe4bfae66dda5b268ee4dbd73eb4
Baljinnyam Puntsagnorov <baljaa367@gmail.com>
2025-11-12 21:28:42 -0500

    Add Comment

--- Frontend/src/pages/MyOrdersPage.jsx
+++ Frontend/src/pages/MyOrdersPage.jsx
@@ -47,6 +47,7 @@ const MyOrdersPage = () => {
   React.useEffect(() => {
     async function fetchArchived() {
       try {
+        //new endpoint to get archived order IDs
         const res = await api.get(`${ORDER_API_ENDPOINT}/archive`);
         setArchivedOrderIds(res.data.archivedOrderIds || []);
       } catch {}
